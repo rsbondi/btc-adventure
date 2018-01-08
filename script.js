2acc9a3f5f4cefd84fb5835f20cce404bbd76576
Richard Bondi <r_bondi@hotmail.com>
2018-01-08 06:32:32 -0600

    this too

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ var Transaction = {
 
       var sigscript = Script.fromSig(scriptbytes)
       var siglen = scriptbytes.slice(0, 1)
-      addSpan('version', 'input ' + i + (sigscript[2] ? ' sig length = ' : ' hash length = ') + siglen, Bytes.toHex(siglen))
+      addSpan('version', 'input ' + i + (sigscript[2] ? ' sig length = ' : ' script length = ') + siglen, Bytes.toHex(siglen))
       addSpan('script', 'input ' + i + (sigscript[2] ? ' signature' : ' redeemScript'), sigscript[0])
       addSpan('version', 'input ' + i + ' signature type', sigscript[1])
 
