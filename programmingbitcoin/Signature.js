b68e1d0545f2ed3f28320711cf9a6048e3f42237
Richard Bondi <r_bondi@hotmail.com>
2018-04-09 09:33:04 -0500

    der

--- programmingbitcoin/Signature.js
+++ programmingbitcoin/Signature.js
@@ -3,6 +3,19 @@ class Signature {
         this.r = r // x coorditate of point R
         this.s = s // s = (z+re)/k
     }
+
+    der() {
+        let rbin = Buffer.from(this.r.toString(16), 'hex')
+        while(!rbin[0]) rbin = rbin.slice(1)
+        if(rbin[0] & 0x80) rbin = Buffer.concat([Buffer.from('00', 'hex'), rbin])
+        let result = Buffer.concat([Buffer.from('02', 'hex'),Buffer.from(rbin.length.toString(16), 'hex'), rbin])
+
+        let sbin = Buffer.from(this.s.toString(16), 'hex')
+        while(!sbin[0]) sbin = sbin.slice(1)
+        if(sbin[0] & 0x80) sbin = Buffer.concat([Buffer.from('00', 'hex'), sbin])
+        result = Buffer.concat([result, Buffer.from('02', 'hex'),Buffer.from(sbin.length.toString(16), 'hex'), sbin])
+        return Buffer.concat([Buffer.from('30', 'hex'), Buffer.from(result.length.toString(16), 'hex'), result])
+    }
 }
 
 module.exports = {
--- programmingbitcoin/ch04.js
+++ programmingbitcoin/ch04.js
@@ -1,5 +1,6 @@
 const { S256Point} = require('./S256Point')
 const { S256Field} = require('./S256Field')
+const { Signature} = require('./Signature')
 const bigInt = require('big-integer')
 
 const px = bigInt("1b1981a0d37c66ad3a35b28a57bdec8d1bd9f4780aa3e9bd4137a0bffa5bb6cf", 16)
@@ -15,3 +16,8 @@ console.log('sec compressed serialized', cser)
 const xCubedPlus7 = pub.x.pow(pub.three).add(pub.b).sqrt()
 const calcy = pub.a.prime.subtract(xCubedPlus7.num) // subtract for 0x02, xCubedPlus7 for 0x03
 console.log('sqrt', calcy.toString(), pub.y.num.toString())
+
+const pr = bigInt("f93441bb92d20b7383732687cf662f3b0aeb1ea4ef6f3db2f745f16ca63c18ed", 16)
+const ps = bigInt("783e6eb62119728c016e30bfec33af09e24a80b0c393b269533c38d04c554503", 16)
+const sig = new Signature(pr, ps)
+console.log('der', sig.der().toString('hex'))
